8253b66de9fbf0bd49bb458cbde64aa99696ebb1
Shoaib <shoaiburrehman4447@gmail.com>
2026-07-06 17:08:51 +0500

    Intro to Drie, useGLTF
    Ready made components for React three fiber,
    glb vs gltf

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,21 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { useRef, useState } from 'react';
-import * as THREE from 'three';
-import { useThree } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { Suspense } from 'react';
 
-// Raycasting is how R3F determines which 3D object your mouse intersects. R3F handles it automatically for pointer events, but understanding it enables advanced interactions.
+// glb vs gltf: glb is a binary format, while gltf is a JSON format. glb is more efficient for transmission and loading, while gltf is more human-readable and easier to edit.
+
+const Model = () => {
+  const { scene, nodes, materials } = useGLTF('/models/car.glb', true);
+  return <primitive object={scene} />;
+};
+
 
 const Box = ({ color, position }: { color: string; position: [number, number, number] }) => {
-  const ref = useRef<THREE.Mesh>(null!);
-  const { raycaster, camera, scene } = useThree()
-
-  const checkIntersections = () => {
-    const intersects = raycaster.intersectObjects(scene.children)
-    if (intersects.length > 0) {
-      console.log('First hit:', intersects[0])
-    }
-  }
-
-
   return (
-    <mesh position={position} onClick={(event) => checkIntersections()} >
+    <mesh position={position}  >
       <boxGeometry />
-      <meshStandardMaterial color={color} roughness={0.1} metalness={0.1} emissive={2}  />
+      <meshStandardMaterial color={color} roughness={0.1} metalness={0.1} emissive={2} />
     </mesh>
   );
 }
@@ -31,19 +24,16 @@ const Box = ({ color, position }: { color: string; position: [number, number, nu
 export default function Home() {
   return (
     <Canvas
-      gl={{ antialias: true }}
-      camera={{ position: [0, 0, 5], fov: 15 }}
-      style={{ width: '100vw', height: '100vh', display: 'block' }}
     >
       //Install react-three/drei for OrbitControls: npm install @react-three/drei
-      <OrbitControls
+      <OrbitControls />
 
-      />
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
 
       {/* Pointer Events (Click and drag to rotate, scroll to zoom, right-click and drag to pan) */}
       <ambientLight intensity={1} />
-      <Box color="red" position={[-1, 0, 0]} />
-      <Box color="blue" position={[1, 0, 0]} />
     </Canvas>
   )
 }
