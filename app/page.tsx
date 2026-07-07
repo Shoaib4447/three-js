de6a0db3c69b3c19526bf20c5b6a858d53c7e5a6
Shoaib <shoaiburrehman4447@gmail.com>
2026-07-07 15:51:00 +0500

    Grouping objects,Perspective Camera, Camera Animations, lookAt , useFrame, Lerp smooth animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,81 +1,53 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
-import React from 'react';
-import { OrbitControls, useGLTF, useTexture, Environment, useHelper } from '@react-three/drei';
-import { Suspense } from 'react';
-import * as THREE from 'three';
-import { DirectionalLightHelper } from 'three'
+import { OrbitControls, Environment, PerspectiveCamera } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
 
-// glb vs gltf: glb is a binary format, while gltf is a JSON format. glb is more efficient for transmission and loading, while gltf is more human-readable and easier to edit.
 
-const Model = () => {
-  const { scene } = useGLTF('/models/car.glb', true);
-  const [colorMap, normalMap, roughnessMap, metalnessMap, aoMap] = useTexture([
-    '/textures/metal/metal_0071_color_1k.jpg',
-    '/textures/metal/metal_0071_normal_opengl_1k.png',
-    '/textures/metal/metal_0071_roughness_1k.jpg',
-    '/textures/metal/metal_0071_metallic_1k.jpg',
-    '/textures/metal/metal_0071_ao_1k.jpg',
-  ]);
+const Scene = () => {
+  const cameraRef = useRef([0, 2, 5]);
+  useFrame((state, delta) => {
+    // You can add animations or interactions here if needed
+    const time = state.clock.elapsedTime;
 
-  scene.traverse((child) => {
-    if ((child as THREE.Mesh).isMesh) {
-      const mesh = child as THREE.Mesh;
-      mesh.material = new THREE.MeshStandardMaterial({
-        map: colorMap,
-        normalMap: normalMap,
-        roughnessMap: roughnessMap,
-        metalnessMap: metalnessMap,
-        aoMap: aoMap,
-        aoMapIntensity: 1,
-
-      });
+    if (time < 3) {
+      cameraRef.current = [0, 2, 5];
+    } else if (time < 6) {
+      cameraRef.current = [5, 2, 0];
+    } else if (time < 9) {
+      cameraRef.current = [0, 2, -5];
+    } else {
+      cameraRef.current = [-5, 2, 0];
     }
+    state.camera.position.x += (cameraRef.current[0] - state.camera.position.x) * delta;
+    state.camera.position.y += (cameraRef.current[1] - state.camera.position.y) * delta;
+    state.camera.position.z += (cameraRef.current[2] - state.camera.position.z) * delta;
+
+
+    state.camera.lookAt(0, 0, 0);
   });
 
-  return <primitive object={scene} scale={[1, 1, 1]} position={[0, -1, 0]} rotation={[0, Math.PI, 0]} />;
-};
 
-const LightHelper = () => {
-  const ref = React.useRef<THREE.DirectionalLight>(null!);
-  useHelper(ref, DirectionalLightHelper, 0.5, 'red');
+
   return (
-    <directionalLight
-      ref={ref}
-      intensity={4}
-      castShadow
-      position={[2, 4, 2]}
-    />
+    <>
+
+      <PerspectiveCamera makeDefault position={[0, 2, 5]} fov={75} />
+      <ambientLight intensity={0.1} />
+      <group>
+        <mesh position={[1, 0, 0]} >
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        <mesh position={[-1, 0, 2]} >
+          <sphereGeometry args={[1, 32, 32]} />
+          <meshStandardMaterial color="purple" />
+        </mesh>
+      </group>
+    </>
   )
 }
-
-const Box = ({ color, position }: { color: string; position: [number, number, number] }) => {
-  // const texture = useTexture('/textures/metal/metal_0071_color_1k.jpg');
-  // const texture2 = useTexture('/textures/metal/metal_0071_normal_opengl_1k.png');
-  // const metalnessMap = useTexture('/textures/metal/metal_0071_metallic_1k.jpg');
-  // const aoMap = useTexture('/textures/metal/metal_0071_ao_1k.jpg');
-  // const roughnessMap = useTexture('/textures/metal/metal_0071_roughness_1k.jpg');
-
-
-  return (
-    <mesh position={position} castShadow >
-      <sphereGeometry />
-      <meshStandardMaterial color={color} roughness={0} metalness={1} />
-    </mesh>
-  );
-}
-
-const Plane = () => {
-  return (
-    <mesh receiveShadow position={[0, -0.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeGeometry args={[100, 100]} />
-      <meshStandardMaterial color="white" />
-    </mesh>
-  );
-}
-
-
-
 
 export default function Home() {
   return (
@@ -83,16 +55,7 @@ export default function Home() {
       shadows
       camera={{ position: [0, 2, 5], fov: 50 }}
     >
-      //Install react-three/drei for OrbitControls: npm install @react-three/drei
-
-
-      <LightHelper />
-      <Box color='white' position={[0, 0, 1]} />
-      {/* <Plane /> */}
-
-      {/* Pointer Events (Click and drag to rotate, scroll to zoom, right-click and drag to pan) */}
-      <ambientLight intensity={0.1} />
-
+      <Scene />
       {/* <spotLight intensity={0.1} position={[0, 0, 0]} angle={1}  /> */}
       <OrbitControls />
       <Environment files={'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/modern_evening_street_1k.hdr'} background />
