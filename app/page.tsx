d57e6dda44cc36132c5b795c39505244d7cb3f79
Shoaib <shoaiburrehman4447@gmail.com>
2026-07-07 16:55:41 +0500

    useMemo usage to create geometries

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Environment, PerspectiveCamera } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useRef,useMemo } from 'react';
+import * as THREE from 'three';
 
 
 const Scene = () => {
@@ -11,22 +12,27 @@ const Scene = () => {
     // You can add animations or interactions here if needed
     const time = state.clock.elapsedTime;
 
-    if (time < 3) {
-      cameraRef.current = [0, 2, 5];
-    } else if (time < 6) {
-      cameraRef.current = [5, 2, 0];
-    } else if (time < 9) {
-      cameraRef.current = [0, 2, -5];
-    } else {
-      cameraRef.current = [-5, 2, 0];
-    }
-    state.camera.position.x += (cameraRef.current[0] - state.camera.position.x) * delta;
-    state.camera.position.y += (cameraRef.current[1] - state.camera.position.y) * delta;
-    state.camera.position.z += (cameraRef.current[2] - state.camera.position.z) * delta;
+    // if (time < 3) {
+    //   cameraRef.current = [0, 2, 5];
+    // } else if (time < 6) {
+    //   cameraRef.current = [5, 2, 0];
+    // } else if (time < 9) {
+    //   cameraRef.current = [0, 2, -5];
+    // } else {
+    //   cameraRef.current = [-5, 2, 0];
+    // }
+    // state.camera.position.x += (cameraRef.current[0] - state.camera.position.x) * delta;
+    // state.camera.position.y += (cameraRef.current[1] - state.camera.position.y) * delta;
+    // state.camera.position.z += (cameraRef.current[2] - state.camera.position.z) * delta;
 
 
-    state.camera.lookAt(0, 0, 0);
+    // state.camera.lookAt(0, 0, 0);
   });
+
+  const geometry = useMemo(() => {
+    const geom = new THREE.BoxGeometry(1, 1, 1);
+    return geom;
+  }, []);
 
 
 
@@ -36,12 +42,14 @@ const Scene = () => {
       <PerspectiveCamera makeDefault position={[0, 2, 5]} fov={75} />
       <ambientLight intensity={0.1} />
       <group>
-        <mesh position={[1, 0, 0]} >
-          <boxGeometry args={[1, 1, 1]} />
+        <mesh position={[1, 0, 0]}  geometry={geometry} >
+          
           <meshStandardMaterial color="white" />
         </mesh>
-        <mesh position={[-1, 0, 2]} >
-          <sphereGeometry args={[1, 32, 32]} />
+        <mesh position={[-1, 0, 2]}  geometry={geometry}>
+          <meshStandardMaterial color="blue" />
+        </mesh>
+        <mesh position={[0, 0, -2]}  geometry={geometry}>
           <meshStandardMaterial color="purple" />
         </mesh>
       </group>
@@ -55,6 +63,8 @@ export default function Home() {
       shadows
       camera={{ position: [0, 2, 5], fov: 50 }}
     >
+      <Scene />
+      <Scene />
       <Scene />
       {/* <spotLight intensity={0.1} position={[0, 0, 0]} angle={1}  /> */}
       <OrbitControls />
