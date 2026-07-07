985441f90a68d0a0b659ac9bcd51bbe5773f4741
Shoaib <shoaiburrehman4447@gmail.com>
2026-07-07 17:10:58 +0500

    memo to avoid re-renders

--- app/page.tsx
+++ app/page.tsx
@@ -2,40 +2,34 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Environment, PerspectiveCamera } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef, useMemo } from 'react';
+import { useRef, useMemo, memo, useState } from 'react';
 import * as THREE from 'three';
 
 
-const Box = ({ color, position }: { color: string, position: [number, number, number] }) => {
-
-  const geometry = useMemo(() => {
-    const geom = new THREE.BoxGeometry(1, 1, 1);
-    return geom;
-  }, []);
+const Box = memo(function Box({ color }: { color: string }) {
   return (
-    <>
-
-      <PerspectiveCamera makeDefault position={[0, 2, 5]} fov={75} />
-      <ambientLight intensity={0.1} />
-      <mesh position={position} geometry={geometry} >
-        <meshStandardMaterial color={color} />
-      </mesh>
-    </>
-  )
-}
+    <mesh position={[0, 0, 0]}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+});
 
 export default function Home() {
+  const [count, setCount] = useState(1);
   return (
-    <Canvas
-      shadows
-      camera={{ position: [0, 2, 5], fov: 50 }}
-    >
-      <Box color={'red'} position={[0, 0, 0]} />
-      <Box color={'green'} position={[2, 0, 0]} />
-      <Box color={'blue'} position={[-2, 0, 0]} />
-      {/* <spotLight intensity={0.1} position={[0, 0, 0]} angle={1}  /> */}
-      <OrbitControls />
-      <Environment files={'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/modern_evening_street_1k.hdr'} background />
-    </Canvas>
+    <>
+      <button onClick={() => setCount(count + 1)}>Click me {count}</button>
+      <Canvas
+        shadows
+        camera={{ position: [0, 2, 5], fov: 75 }}
+      >
+        <Box color={'red'} />
+        <ambientLight intensity={2} />
+        {/* <spotLight intensity={0.1} position={[0, 0, 0]} angle={1}  /> */}
+        <OrbitControls />
+        {/* <Environment files={'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/modern_evening_street_1k.hdr'} blur={0} background /> */}
+      </Canvas>
+    </>
   )
 }
