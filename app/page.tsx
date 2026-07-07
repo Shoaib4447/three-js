98114445857ee270b3dd7637e3aafb8b4b4e19bb
Shoaib <shoaiburrehman4447@gmail.com>
2026-07-07 17:00:29 +0500

    useMemo to memize the geometry

--- app/page.tsx
+++ app/page.tsx
@@ -2,57 +2,24 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Environment, PerspectiveCamera } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef,useMemo } from 'react';
+import { useRef, useMemo } from 'react';
 import * as THREE from 'three';
 
 
-const Scene = () => {
-  const cameraRef = useRef([0, 2, 5]);
-  useFrame((state, delta) => {
-    // You can add animations or interactions here if needed
-    const time = state.clock.elapsedTime;
-
-    // if (time < 3) {
-    //   cameraRef.current = [0, 2, 5];
-    // } else if (time < 6) {
-    //   cameraRef.current = [5, 2, 0];
-    // } else if (time < 9) {
-    //   cameraRef.current = [0, 2, -5];
-    // } else {
-    //   cameraRef.current = [-5, 2, 0];
-    // }
-    // state.camera.position.x += (cameraRef.current[0] - state.camera.position.x) * delta;
-    // state.camera.position.y += (cameraRef.current[1] - state.camera.position.y) * delta;
-    // state.camera.position.z += (cameraRef.current[2] - state.camera.position.z) * delta;
-
-
-    // state.camera.lookAt(0, 0, 0);
-  });
+const Box = ({ color, position }: { color: string, position: [number, number, number] }) => {
 
   const geometry = useMemo(() => {
     const geom = new THREE.BoxGeometry(1, 1, 1);
     return geom;
   }, []);
-
-
-
   return (
     <>
 
       <PerspectiveCamera makeDefault position={[0, 2, 5]} fov={75} />
       <ambientLight intensity={0.1} />
-      <group>
-        <mesh position={[1, 0, 0]}  geometry={geometry} >
-          
-          <meshStandardMaterial color="white" />
-        </mesh>
-        <mesh position={[-1, 0, 2]}  geometry={geometry}>
-          <meshStandardMaterial color="blue" />
-        </mesh>
-        <mesh position={[0, 0, -2]}  geometry={geometry}>
-          <meshStandardMaterial color="purple" />
-        </mesh>
-      </group>
+      <mesh position={position} geometry={geometry} >
+        <meshStandardMaterial color={color} />
+      </mesh>
     </>
   )
 }
@@ -63,9 +30,9 @@ export default function Home() {
       shadows
       camera={{ position: [0, 2, 5], fov: 50 }}
     >
-      <Scene />
-      <Scene />
-      <Scene />
+      <Box color={'red'} position={[0, 0, 0]} />
+      <Box color={'green'} position={[2, 0, 0]} />
+      <Box color={'blue'} position={[-2, 0, 0]} />
       {/* <spotLight intensity={0.1} position={[0, 0, 0]} angle={1}  /> */}
       <OrbitControls />
       <Environment files={'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/modern_evening_street_1k.hdr'} background />
